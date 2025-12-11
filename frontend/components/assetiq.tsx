0c0d7a2eafd6ea543819f64d52201589be434860
santhoshnagendra838-maker <santhosh.nagendra838@gmail.com>
2025-12-11 13:32:20 +0530

    feat: correct url for backend

--- frontend/components/assetiq.tsx
+++ frontend/components/assetiq.tsx
@@ -52,7 +52,7 @@ export default function InstrumentComparisonUI() {
       setComparison({
         a: instrumentA,
         b: instrumentB,
-        response: "Error: Unable to fetch comparison data. Please ensure the backend server is running on http://assetiq-test-alb-1291527958.ap-south-1.elb.amazonaws.com"
+        response: "Error: Unable to fetch comparison data. Please ensure the backend server is running on http://assetiq-test-alb-88125757.ap-south-1.elb.amazonaws.com/"
       });
     } finally {
       setLoading(false);
